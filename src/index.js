06ae09215659b9afbf99bf60af961d2a614fabcf
Rodrigo Cavero Blades <rodrigo.a.cavero.blades.21@dartmouth.edu>
2020-04-16 19:45:50 -0400

    pushing all files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("starting up!");
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+let num = 0;
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds`);
+  num += 1;
+}, 1000);
